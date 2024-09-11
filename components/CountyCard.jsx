99dd2928150a3638aad13d1cecbbed4fa5bd4563
chinturishi <rishi_kanta@hotmail.com>
2024-09-11 23:00:22 +0530

    bug fix https://github.com/chinturishi/countries-details/issues/1

--- components/CountyCard.jsx
+++ components/CountyCard.jsx
@@ -6,7 +6,9 @@ const CountyCard = ({name,population,region,capital,flag}) => {
         <div className="card-text">
           <h3 className="card-title">{name}</h3>
           <p>
-            <b>Population: </b>{population}
+            <b>Population: </b>{population.toLocaleString(
+                'en-IN'
+              )}
           </p>
           <p>
             <b>Region: </b>{region}
