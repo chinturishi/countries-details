620103ff5c29a9011fdbc4da87be08d7b6cce030
chinturishi <rishi_kanta@hotmail.com>
2024-09-11 23:50:07 +0530

    Implemented search feature

--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -1,35 +1,26 @@
 import CountyCard from "./CountyCard";
 import countiesdata from "../countiesdata";
 
-const CountriesList = () => {
-  //console.log(countiesdata);
-  // const list = countiesdata.map((country) => {
-  //   return (
-  //     <CountyCard
-  //     key={country.name.common}
-  //       name={country.name.common}
-  //       population={country.population}
-  //       region={country.region}
-  //       capital={country.capital}
-  //       flag={country.flags.svg}
-  //     />
-  //   );
-  // });
-  // console.log(list);
+const CountriesList = ({ query }) => {
   return (
     <div className="countries-container">
-      {countiesdata.map((country) => {
-        return (
-          <CountyCard
-            key={country.name.common}
-            name={country.name.common}
-            population={country.population}
-            region={country.region}
-            capital={country.capital}
-            flag={country.flags.svg}
-          />
-        );
-      })}
+      {countiesdata
+        .filter((country) =>
+          country.name.common.toLocaleLowerCase().includes(query)
+        )
+        .map((country) => {
+          console.log(country);
+          return (
+            <CountyCard
+              key={country.name.common}
+              name={country.name.common}
+              population={country.population}
+              region={country.region}
+              capital={country.capital}
+              flag={country.flags.svg}
+            />
+          );
+        })}
     </div>
   );
 };
