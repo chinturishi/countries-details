10afc026edac543afd1d70cfddcc54ae42658ec1
chinturishi <rishi_kanta@hotmail.com>
2024-09-11 23:24:03 +0530

    Modified CountriesList component

--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -3,19 +3,35 @@ import countiesdata from "../countiesdata";
 
 const CountriesList = () => {
   //console.log(countiesdata);
-  const list = countiesdata.map((country) => {
-    return (
-      <CountyCard
-        name={country.name.common}
-        population={country.population}
-        region={country.region}
-        capital={country.capital}
-        flag={country.flags.svg}
-      />
-    );
-  });
-  console.log(list);
-  return <div className="countries-container">{list}</div>;
+  // const list = countiesdata.map((country) => {
+  //   return (
+  //     <CountyCard
+  //     key={country.name.common}
+  //       name={country.name.common}
+  //       population={country.population}
+  //       region={country.region}
+  //       capital={country.capital}
+  //       flag={country.flags.svg}
+  //     />
+  //   );
+  // });
+  // console.log(list);
+  return (
+    <div className="countries-container">
+      {countiesdata.map((country) => {
+        return (
+          <CountyCard
+            key={country.name.common}
+            name={country.name.common}
+            population={country.population}
+            region={country.region}
+            capital={country.capital}
+            flag={country.flags.svg}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default CountriesList;
--- components/CountyCard.jsx
+++ components/CountyCard.jsx
@@ -1,7 +1,7 @@
 const CountyCard = ({name,population,region,capital,flag}) => {
   return (
     <div className="country-card">
-      <a  href="/country.html?name=South Georgia">
+      <a href={`/country.html?name=${name}`}>
         <img src={flag} alt={name} />
         <div className="card-text">
           <h3 className="card-title">{name}</h3>
@@ -14,7 +14,7 @@ const CountyCard = ({name,population,region,capital,flag}) => {
             <b>Region: </b>{region}
           </p>
           <p>
-            <b>Capital: </b>{capital}
+            <b>Capital: </b>{capital?.[0]}
           </p>
         </div>
       </a>
