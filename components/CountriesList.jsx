6d9662460f2a65876c28c3ae33d54fbb2c2a32d5
chinturishi <rishi_kanta@hotmail.com>
2024-09-16 20:33:55 +0530

    https://github.com/chinturishi/countries-details/issues/5 bug-fix

--- components/CountriesList.jsx
+++ components/CountriesList.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import CountyCard from "./CountyCard";
 //import countiesdata from "../countiesdata";
 
-const CountriesList = ({ query,region }) => {
+const CountriesList = ({ query }) => {
   const [countiesData, setcountiesData] = useState([]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -15,11 +15,48 @@ const CountriesList = ({ query,region }) => {
   return (
     countiesData.length === 0 ? "Loading........" :
     
+    // <div className="countries-container">
+    //   {
+    //     region === "" ? countiesData
+    //     .filter((country) =>
+    //       country.name.common.toLocaleLowerCase().includes(query) || country.region.toLocaleLowerCase().includes(query)
+    //     )
+    //     .map((country) => {
+    //       //console.log(country);
+    //       return (
+    //         <CountyCard
+    //           key={country.name.common}
+    //           name={country.name.common}
+    //           population={country.population}
+    //           region={country.region}
+    //           capital={country.capital}
+    //           flag={country.flags.svg}
+    //           data={country}
+    //         />
+    //       );
+    //     }) : countiesData.filter((country) =>
+    //       country.region === region
+    //     ).map((country) => {
+    //       //console.log(country);
+    //       return (
+    //         <CountyCard
+    //           key={country.name.common}
+    //           name={country.name.common}
+    //           population={country.population}
+    //           region={country.region}
+    //           capital={country.capital}
+    //           flag={country.flags.svg}
+    //         />
+    //       );
+    //     })
+    //   }
+    // </div>
+
     <div className="countries-container">
       {
-        region === "" ? countiesData
+        countiesData
         .filter((country) =>
-          country.name.common.toLocaleLowerCase().includes(query)
+          country.name.common.toLocaleLowerCase().includes(query) || country.region.toLocaleLowerCase().includes(query)
         )
         .map((country) => {
           //console.log(country);
@@ -32,20 +69,6 @@ const CountriesList = ({ query,region }) => {
               capital={country.capital}
               flag={country.flags.svg}
               data={country}
-            />
-          );
-        }) : countiesData.filter((country) =>
-          country.region === region
-        ).map((country) => {
-          //console.log(country);
-          return (
-            <CountyCard
-              key={country.name.common}
-              name={country.name.common}
-              population={country.population}
-              region={country.region}
-              capital={country.capital}
-              flag={country.flags.svg}
             />
           );
         })
